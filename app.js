499989cc9a1dba23ab680548bc1225b9682b0052
Benjamin Eceiza <benjaceiza400@gmail.com>
2026-04-13 17:33:20 -0300

    Construir página de inicio

--- app.js
+++ app.js
@@ -4,42 +4,45 @@ const port = 3000;
 const app = express();
 
 app.set("view engine", "ejs");
-    
+
 app.use(express.static('public'));
 
+
+const categorias = require('./public/js/categories');
+const products = require('./public/js/products');
+
+
 // Rutas
-
-
 //Pagina de inicio
-app.get('/', 
-	(req, res) => res.render('pages/index')
+app.get('/',
+    (req, res) => res.render('pages/index',{categorias, products})
 );
 
 //Página de un producto en particular
-app.get('/products', 
-	(req, res) => res.render('pages/product')
+app.get('/products',
+    (req, res) => res.render('pages/product')
 );
 
 //Página del carrito de compras
-app.get('/cart', 
-	(req, res) => res.render('pages/cart')
+app.get('/cart',
+    (req, res) => res.render('pages/cart')
 );
 
 //Página de pago
-app.get('/checkout', 
-	(req, res) => res.render('pages/checkout')
+app.get('/checkout',
+    (req, res) => res.render('pages/checkout')
 );
 
 //Página de registro para new users (register.ejs)
-app.get('/register', 
-	(req, res) => res.render('pages/register')
+app.get('/register',
+    (req, res) => res.render('pages/register')
 );
 
 //Página de inicio de sesión (login.ejs)
-app.get('/login', 
-	(req, res) => res.render('pages/login')
+app.get('/login',
+    (req, res) => res.render('pages/login')
 );
 
 app.listen(port, () => {
-  console.log(`Servidor corriendo en el puerto ${port}`);
+    console.log(`Servidor corriendo en el puerto ${port}`);
 });
---
+++ public/js/products.js
@@ -0,0 +1,103 @@
+
+
+const products = [
+    {
+        id: 1,
+        name: "Lata de Coca-Cola",
+        price: 1.50,
+        image: "/images/products/coca.jfif",
+        description: "Una refrescante lata de Coca-Cola para disfrutar en cualquier momento.",
+        categoria:"bebida"
+    },
+    {
+        id: 2,
+        name: "Lata de Pepsi",
+        price: 1.50,
+        image: "/images/products/pepsi.png",
+        description: "Una refrescante lata de Pepsi para disfrutar en cualquier momento.",
+        categoria:"bebida"
+    },
+    {
+        id: 3,
+        name: "Lata de Red Bull",
+        price: 2.00,
+        image: "/images/products/redbull.avif",
+        description: "Una energética lata de Red Bull para darle más energía a tu día.",
+        categoria:"bebida"
+    },
+    {
+        id: 4,
+        name: "Lata de Monster",
+        price: 2.00,
+        image: "/images/products/mosnter.webp",
+        description: "Una energética lata de Monster para darle más energía a tu día.",
+        categoria:"bebida"
+    },
+    {
+        id: 5,
+        name: "Botella de jack daniels",
+        price: 25.00,
+        image: "/images/products/jack.jfif",
+        description: "Una botella de jack daniels para disfrutar en cualquier momento.",
+        categoria:"alcohol"
+    },
+    {
+        id: 6,
+        name: "Botella de Blue Label",
+        price: 30.00,
+        image: "/images/products/blue-label.webp",
+        description: "Una botella de Blue Label para disfrutar en cualquier momento.",
+        categoria:"alcohol"
+    },
+    {
+        id: 7,
+        name: "Botella de Smirnoff",
+        price: 20.00,
+        image: "/images/products/smirnoff.jfif",
+        description: "Una botella de Smirnoff para disfrutar en cualquier momento.",
+        categoria:"alcohol"
+    },
+    {
+        id: 8,
+        name: "Botella de Fernet Branca",
+        price: 25.00,
+        image: "/images/products/fernet.jfif",
+        description: "Una botella de Fernet Branca para disfrutar en cualquier momento.",
+        categoria:"alcohol"
+    },
+    {
+        id: 9,
+        name: "Bolsa de papas fritas Lay's",
+        price: 5.00,
+        image: "/images/products/lays.jfif",
+        description: "Una bolsa de papas fritas Lay's para disfrutar en cualquier momento.",
+        categoria:"comida"
+    },
+    {
+        id: 10,
+        name: "Bolsa de papas fritas Pringles",
+        price: 5.00,
+        image: "/images/products/pringles.jfif",
+        description: "Una bolsa de papas fritas Pringles para disfrutar en cualquier momento.",
+        categoria:"comida"
+    },
+    {
+        id: 11,
+        name: "Bolsa de Doritos",
+        price: 5.00,
+        image: "/images/products/doritos.jfif",
+        description: "Una bolsa de Doritos para disfrutar en cualquier momento.",
+        categoria:"comida"
+    },
+    {
+        id: 12,
+        name: "Bolsa de Cheetos",
+        price: 5.00,
+        image: "/images/products/cheetos.jpg",
+        description: "Una bolsa de Cheetos para disfrutar en cualquier momento.",
+        categoria:"comida"
+    }
+
+]
+
+module.exports = products;
